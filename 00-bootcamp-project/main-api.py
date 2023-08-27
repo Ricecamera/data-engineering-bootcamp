e4e5356160de64516a656a4d369bbda0edcbfff3
Sahatsarin Pawanna <58083752+Ricecamera@users.noreply.github.com>
2023-08-27 02:26:35 +0000

    update day 3 workshop

--- 00-bootcamp-project/main-api.py
+++ 00-bootcamp-project/main-api.py
@@ -12,44 +12,42 @@ port = parser.get("api_config", "port")
 API_URL = f"http://{host}:{port}"
 DATA_FOLDER = "data"
 
-### Events
-data = "events"
-date = "2021-02-10"
-response = requests.get(f"{API_URL}/{data}/?created_at={date}")
-data = response.json()
-with open(f"{DATA_FOLDER}/ev ents.csv", "w") as f:
-    writer = csv.writer(f)
-    header = data[0].keys()
-    writer.writerow(header)
+tables = ["events", "users", "orders"]
+dates = ["2021-02-10", "2020-10-23", "2021-02-10"]
 
-    for each in data:
-        writer.writerow(each.values())
+for table, date in zip(tables, dates):
+    response = requests.get(f"{API_URL}/{table}/?created_at={date}")
+    data = response.json()
+    with open(f"{DATA_FOLDER}/{table}.csv", "w") as f:
+        writer = csv.writer(f)
+        header = data[0].keys()
+        writer.writerow(header)
 
-### Users
-data = "users"
-date = "2020-10-23"
-# ลองดึงข้อมูลจาก API เส้น users และเขียนลงไฟล์ CSV
-# YOUR CODE HERE
-response = requests.get(f"{API_URL}/{data}/?created_at={date}")
-data = response.json()
-with open(f"{DATA_FOLDER}/users.csv", "w")  as f:
-    writer = csv.writer(f)
-    header = data[0].keys()
-    writer.writerow(header)
+        for each in data:
+            writer.writerow(each.values())
 
-    for each in data:
-        writer.writerow(each.values())
-### Orders
-data = "orders"
-date = "2021-02-10"
-# ลองดึงข้อมูลจาก API เส้น orders และเขียนลงไฟล์ CSV
-# YOUR CODE HERE
-response = requests.get(f"{API_URL}/{data}/?created_at={date}")
-data = response.json()
-with open(f"{DATA_FOLDER}/orders.csv", "w")  as f:
-    writer = csv.writer(f)
-    header = data[0].keys()
-    writer.writerow(header)
 
-    for each in data:
-        writer.writerow(each.values())
+# # ลองดึงข้อมูลจาก API เส้น users และเขียนลงไฟล์ CSV
+# # YOUR CODE HERE
+# response = requests.get(f"{API_URL}/{data}/?created_at={date}")
+# data = response.json()
+# with open(f"{DATA_FOLDER}/users.csv", "w")  as f:
+#     writer = csv.writer(f)
+#     header = data[0].keys()
+#     writer.writerow(header)
+
+#     for each in data:
+#         writer.writerow(each.values())
+
+
+# # ลองดึงข้อมูลจาก API เส้น orders และเขียนลงไฟล์ CSV
+# # YOUR CODE HERE
+# response = requests.get(f"{API_URL}/{data}/?created_at={date}")
+# data = response.json()
+# with open(f"{DATA_FOLDER}/orders.csv", "w")  as f:
+#     writer = csv.writer(f)
+#     header = data[0].keys()
+#     writer.writerow(header)
+
+#     for each in data:
+#         writer.writerow(each.values())
